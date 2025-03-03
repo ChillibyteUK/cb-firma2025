e6787ea9662211a35d9f602f9e17cb8acfd2d55b
ChillibyteUK <83024977+ChillibyteUK@users.noreply.github.com>
2025-03-03 09:29:43 +0000

    text image and feature

--- page-templates/blocks/cb_feature_section.php
+++ page-templates/blocks/cb_feature_section.php
@@ -1,15 +1,49 @@
 <?php
-$img = wp_get_attachment_image_url(get_field('background'), 'full');// ?? null;
+$background = get_field('background') ?: '';
+$underline = !empty(get_field('underline')[0]) ? 'underline' : '';
+$pattern = !empty(get_field('pattern')[0]) ?: '';
+$pbg = $pattern ? ($background == 'Blue' ? 'has-tile-bg has-tile-bg--wo has-tile-bg--right' : 'has-tile-bg has-tile-bg--right') : null;
+$device = get_field('device')[0] ?: '';
+
+$text = '';
+$dd = 'device-primary.svg';
+// $pbg = $pattern ?
+
+if ($background == 'Blue') {
+    $text = 'text-white';
+    $background = 'py-5 has-primary-400-background-color';
+    $dd = 'device-wo.svg';
+}
+
 ?>
-<section class="feature_section" style="background-image:url(<?=$img?>);">
+<section class="feature_section <?=$background?> <?=$pbg?>">
     <div class="container-xl py-5">
-        <div class="row g-4">
+        <div class="row g-5">
             <div class="col-md-6">
-                <h2 class="underline mb-4">
-                    <?=get_field('title')?>
-                </h2>
+                <?php
+                if ($device) {
+                    ?>
+                    <div class="row g-4">
+                        <div class="col-2">
+                            <img src="<?=get_stylesheet_directory_uri()?>/img/<?=$dd?>" alt="">
+                        </div>
+                        <div class="col-10">
+                            <h2 class="mb-4 <?=$text?> <?=$underline?>">
+                                <?=get_field('title')?>
+                            </h2>
+                        </div>
+                    </div>
+                    <?php
+                } else {
+                    ?>
+                    <h2 class="mb-4 <?=$text?> <?=$underline?>">
+                        <?=get_field('title')?>
+                    </h2>
+                    <?php
+                }
+                ?>
             </div>
-            <div class="col-md-6" data-aos="fade">
+            <div class="col-md-6 <?=$text?>" data-aos="fade">
                 <?=get_field('content')?>
             </div>
         </div>
--- page-templates/blocks/cb_text_image_feature.php
+++ page-templates/blocks/cb_text_image_feature.php
@@ -1,13 +1,56 @@
 <?php
+$background = get_field('background') ?: '';
+$overlay = get_field('overlay') ?: '';
+$underline = !empty(get_field('underline')[0]) ? 'underline' : '';
+$pattern = $overlay ? ($background == 'Blue' ? 'text_image_feature__overlay--wo' : 'text_image_feature__overlay') : null;
+$device = !empty(get_field('device')[0]) ?: '';
+
+$text = '';
+$dd = 'device-primary.svg';
+
+if ($background == 'Blue') {
+    $text = 'text-white';
+    $background = 'py-5 has-primary-400-background-color';
+    $dd = 'device-wo.svg';
+}
+
+var_dump($background);
+var_dump($underline);
+var_dump($overlay);
+var_dump($pattern);
+var_dump($device);
+
 $order_left = (get_field('order') == 'text') ? 'order-1 order-lg-1' : 'order-1 order-lg-2';
 $order_right = (get_field('order') == 'text') ? 'order-2 order-lg-2' : 'order-2 order-lg-1';
 ?>
-<section class="text_image_feature">
+<section class="text_image_feature <?=$background?>">
     <div class="container-xl py-5">
-        <div class="row g-4">
+        <div class="row g-5">
             <div class="col-md-6 d-flex flex-column justify-content-center text_image_feature__content <?=$order_left?>">
-                <h2 class="underline mb-4"><?=get_field('title')?></h2>
-                <p data-aos="fade" class="mb-4"><?=get_field('content')?></p>
+            <?php
+                if ($device) {
+                    ?>
+                    <div class="row g-4">
+                        <div class="col-2">
+                            <img src="<?=get_stylesheet_directory_uri()?>/img/<?=$dd?>" alt="">
+                        </div>
+                        <div class="col-10">
+                            <h2 class="mb-4 <?=$text?> <?=$underline?>">
+                                <?=get_field('title')?>
+                            </h2>
+                            <p data-aos="fade" class="mb-4 <?=$text?>"><?=get_field('content')?></p>
+                        </div>
+                    </div>
+                    <?php
+                } else {
+                    ?>
+                    <h2 class="mb-4 <?=$text?> <?=$underline?>">
+                        <?=get_field('title')?>
+                    </h2>
+                    <p data-aos="fade" class="mb-4 <?=$text?>"><?=get_field('content')?></p>
+                    <?php
+                }
+                ?>
                 <?php
                 if(get_field('cta')) {
                     $l = get_field('cta');
@@ -17,8 +60,9 @@ $order_right = (get_field('order') == 'text') ? 'order-2 order-lg-2' : 'order-2 
                 }
                 ?>
             </div>
-            <div class="col-lg-6 <?=$order_right?>">
-                <div class="text_image_feature__image" style="background-image:url(<?=wp_get_attachment_image_url(get_field('image'),'full')?>)"></div>
+            <div class="col-lg-6 <?=$order_right?> text_image_feature__container">
+                <?=wp_get_attachment_image(get_field('image'),'full',false,['class' => 'text_image_feature__image'])?>
+                <div class="<?=$pattern?>"></div>
             </div>
         </div>
     </div>
