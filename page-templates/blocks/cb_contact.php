6f32e1bf0a90a442708bbe7b617d98624582b755
ChillibyteUK <dan@chillibyte.co.uk>
2025-03-11 13:55:44 +0000

    contact

--- page-templates/blocks/cb_contact.php
+++ page-templates/blocks/cb_contact.php
@@ -2,12 +2,21 @@
     <div class="container-xl pb-5">
         <div class="row g-4">
             <div class="col-md-6">
-                <h2>Contact Us</h2>
-                <ul class="fa-ul">
-                    <li class="mb-4"><span class="fa-li"><i class="fa-solid fa-phone has-accent-400-color"></i></span> <a href="tel:<?=parse_phone(get_field('contact_phone', 'options'))?>"><?=get_field('contact_phone', 'options')?></a></li>
-                    <li class="mb-4"><span class="fa-li"><i class="fa-solid fa-envelope has-accent-400-color"></i></span> <a href="mailto:<?=get_field('contact_email', 'options')?>"><?=get_field('contact_email', 'options')?></a></li>
-                    <li class="mb-4"><span class="fa-li"><i class="fa-solid fa-map-marker-alt has-accent-400-color"></i></span> <?=get_field('contact_address', 'options')?></li>
-                </ul>
+                <div class="row g-4">
+                    <div class="col-2">
+                        <img src="<?=get_stylesheet_directory_uri()?>/img/device-primary.svg" alt="">
+                    </div>
+                    <div class="col-10">
+                        <h2 class="mb-4">
+                            Contact Us
+                        </h2>
+                        <ul class="fa-ul>
+                            <li class="mb-4"><span class="fa-li"><i class="fa-solid fa-phone has-accent-400-color"></i></span> <a href="tel:<?=parse_phone(get_field('contact_phone', 'options'))?>"><?=get_field('contact_phone', 'options')?></a></li>
+                            <li class="mb-4"><span class="fa-li"><i class="fa-solid fa-envelope has-accent-400-color"></i></span> <a href="mailto:<?=get_field('contact_email', 'options')?>"><?=get_field('contact_email', 'options')?></a></li>
+                            <li class="mb-4"><span class="fa-li"><i class="fa-solid fa-map-marker-alt has-accent-400-color"></i></span> <?=get_field('contact_address', 'options')?></li>
+                        </ul>
+                    </div>
+                </div>
             </div>
             <div class="col-md-6 contact__container">
                 <?=wp_get_attachment_image(get_field('image'), 'full', false, ['class' => 'contact__image'])?>
