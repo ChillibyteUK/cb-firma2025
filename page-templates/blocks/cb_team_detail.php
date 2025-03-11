8f106c6c78064b610bf28e0e92acdf6f43646425
ChillibyteUK <dan@chillibyte.co.uk>
2025-03-11 17:24:46 +0000

    team detail hover

--- page-templates/blocks/cb_team_detail.php
+++ page-templates/blocks/cb_team_detail.php
@@ -25,34 +25,33 @@ while ($q->have_posts()) {
     $slug = acf_slugify(get_the_title());
     ?>
         <a class="anchor" id="<?=$slug?>"></a>
-            <div class="row g-4 mb-4">
-                <div class="col-md-2">
-                    <img class="team_detail__image" src="<?=get_the_post_thumbnail_url($q->ID, 'large')?>" alt="<?=get_the_title()?>">
-                </div>
-                <div class="col-md-10">
-                    <div class="d-flex flex-wrap justify-content-between align-items-center">
-                        <h2 class="team_detail__name"><?=get_the_title()?></h2>
+        <div class="row team_detail__row g-4 mb-4">
+            <div class="col-md-2">
+                <img class="team_detail__image" src="<?=get_the_post_thumbnail_url($q->ID, 'large')?>" alt="<?=get_the_title()?>">
+            </div>
+            <div class="col-md-10">
+                <div class="d-flex flex-wrap justify-content-between align-items-center">
+                    <h2 class="team_detail__name"><?=get_the_title()?></h2>
 
-                        <div class="team_detail__meta">
-                            <?php
-                    if (get_field('email_address', get_the_ID())) {
-                        ?>
-                            <a href="mailto:<?=get_field('email_address', get_the_ID())?>"><i class="fa-solid fa-envelope"></i></a>
-                            <?php
-                    }
+                    <div class="team_detail__meta">
+                        <?php
+                if (get_field('email_address', get_the_ID())) {
+                    ?>
+                        <a href="mailto:<?=get_field('email_address', get_the_ID())?>"><i class="fa-solid fa-envelope"></i></a>
+                        <?php
+                }
     if (get_field('linkedin_profile', get_the_ID())) {
         ?>
-                            <a href="<?=get_field('linkedin_profile', get_the_ID())?>" target="_blank"><i class="fa-brands fa-linkedin-in"></i></a>
-                            <?php
+                        <a href="<?=get_field('linkedin_profile', get_the_ID())?>" target="_blank"><i class="fa-brands fa-linkedin-in"></i></a>
+                        <?php
     }
     ?>
-                        </div>
                     </div>
-                    <div class="team_detail__role"><?=get_field('role', get_the_ID())?></div>
-                    <div class="mb-4"><?=get_field('biography', get_the_ID())?></div>
                 </div>
+                <div class="team_detail__role"><?=get_field('role', get_the_ID())?></div>
+                <div class="mb-4"><?=get_field('biography', get_the_ID())?></div>
             </div>
-        </a>
+        </div>
         <hr class="my-4">
                 <?php
 }
