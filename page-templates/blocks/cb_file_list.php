493dc8aec2e2d752aa5af91db6402c61685a0300
ChillibyteUK <dan@chillibyte.co.uk>
2025-04-02 10:49:19 +0000

    file list

--- page-templates/blocks/cb_file_list.php
+++ page-templates/blocks/cb_file_list.php
@@ -7,11 +7,11 @@
 
 if ( have_rows( 'files' ) ) {
 	?>
-	<div class="container-xl">
+<section class="file_list">
+	<div class="container-xl py-5">
 		<div class="row">
 			<div class="col-12">
-				<h2 class="text-center">Files</h2>
-				<ul class="list-unstyled">
+				<div class="row g-5 justify-content-center">
 					<?php
 					while ( have_rows( 'files' ) ) {
 						the_row();
@@ -24,30 +24,32 @@ if ( have_rows( 'files' ) ) {
 						$url        = esc_url( $file['url'] );
 						$file_title = esc_html( $file['title'] );
 						$size       = size_format( $file['filesize'], 2 );
-						$icon_url   = wp_mime_type_icon( $file['ID'] ); // gets the icon for the file
+						$icon_url   = wp_mime_type_icon( $file['ID'] ); // gets the icon for the file.
 						$thumbnail  = wp_get_attachment_image_src( $file['ID'], 'medium' );
 
-						
 						?>
-						<li class="d-flex align-items-center gap-2 mb-3">
-							<a href="<?= esc_url( $url ); ?>" target="_blank" rel="noopener noreferrer">
-								<?php
-								if ( $thumbnail ) {
-									echo '<img src="' . esc_url( $thumbnail[0] ) . '" alt="' . esc_attr( $file['title'] ) . '" loading="lazy">';
-								} else {
-									echo '<img src="' . esc_url( wp_mime_type_icon( $file['ID'] ) ) . '" alt="" width="32" height="32">';
-								}
-								?>
-								<?= esc_html( $file_title ); ?>
+						<div class="col-sm-6 col-md-4">
+							<a href="<?= esc_url( $url ); ?>" target="_blank" rel="noopener noreferrer" class="file_list__card">
+								<div class="file_list__inner">
+									<?php
+									if ( $thumbnail ) {
+										echo '<img src="' . esc_url( $thumbnail[0] ) . '" alt="' . esc_attr( $file['title'] ) . '" loading="lazy" class="file_list__image">';
+									} else {
+										echo '<img src="' . esc_url( wp_mime_type_icon( $file['ID'] ) ) . '" alt="" width="132" height="132" class="file_list__image">';
+									}
+									?>
+									<div class="h4"><?= esc_html( $file_title ); ?></div>
+									<span class="text-muted fs-100">(<?= esc_html( $size ); ?>)</span>
+								</div>
 							</a>
-							<span class="text-muted ms-2 small">(<?= esc_html( $size ); ?>)</span>
-						</li>
+							</div>
 						<?php
 					}
 					?>
-				</ul>
+				</div>
 			</div>
 		</div>
 	</div>
+</section>
 	<?php
 }
