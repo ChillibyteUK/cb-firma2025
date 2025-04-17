0aea54145ee36c943669e1da524ec7a2bd007456
Matthew Pont <matthew.pont@gmail.com>
2025-04-17 09:17:05 +0100

    Update single-press.php

--- single-press.php
+++ single-press.php
@@ -20,7 +20,18 @@ $img = get_the_post_thumbnail_url( get_the_ID(), 'full' );
 		</section>
 		<article>
 			<h1><?= esc_html( get_the_title() ); ?></h1>
-			<?= wp_kses_post( get_the_content() ); ?>
+			<div class="row">
+				<div class="col-md-4 order-md-2">
+                    <?php 
+                    $image = get_field('image');
+                    if( !empty( $image ) ): ?>
+                        <img src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" class="img-fluid mb-3" />
+                    <?php endif; ?>
+				</div>
+				<div class="col-md-8 order-md-1">
+					<?= wp_kses_post( get_the_content() ); ?>
+				</div>
+			</div>
 		</article>
     </div>
 	<?php
