5fcccf171a54704cb2f0d6d2384b166561dd6ba4
Matthew Pont <matthew.pont@gmail.com>
2025-04-22 09:12:25 +0100

    Update single-press.php

--- single-press.php
+++ single-press.php
@@ -29,7 +29,7 @@ $img = get_the_post_thumbnail_url( get_the_ID(), 'full' );
 				</div>
 				<div class="col-md-8 order-md-1">
 					<h1><?= esc_html( get_the_title() ); ?></h1>
-					<div class="mb-2 fs-7" style="font-size: 0.8rem;"><?php echo get_the_date('jS F Y'); ?></div>
+					<div class="mb-2" style="font-size: 0.8rem;"><?php echo get_the_date('jS F Y'); ?></div>
 					<?= wp_kses_post( get_the_content() ); ?>
 				</div>
 			</div>
