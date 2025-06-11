3c70a21644a24622a50b9b8b62fc99de3fd363a3
ChillibyteUK <dan@chillibyte.co.uk>
2025-06-11 11:48:37 +0100

    video hero title hierarch

--- page-templates/blocks/cb_video_hero.php
+++ page-templates/blocks/cb_video_hero.php
@@ -20,15 +20,28 @@ $webm  = get_field( 'video_webm' ) ?? null;
     <div class="container-xl d-flex flex-column justify-content-center">
         <div class="row g-4">
             <div class="col-md-6">
+				<?php
+				$block_title = get_field( 'title' );
+				$subtitle    = get_field( 'subtitle' );
+
+				if ( ! $block_title ) {
+					?>
                 <h1 data-aos="fade">
-                    <?= esc_html( get_field( 'title' ) ); ?>
+                    <?= esc_html( $subtitle ); ?>
                 </h1>
-                <?php
-                if ( get_field( 'subtitle' ) ) {
-                    ?>
-                <div class="text-white" data-aos="fade"><?= wp_kses_post( get_field( 'subtitle' ) ); ?></div>
-                    <?php
-                }
+					<?php
+				} else {
+					?>
+					<h1 class="fs-400" data-aos="fade">
+						<?= esc_html( get_field( 'title' ) ); ?>
+					</h1>
+					<?php
+					if ( get_field( 'subtitle' ) ) {
+						?>
+					<h2 class="h1 text-white" data-aos="fade"><?= wp_kses_post( $subtitle ); ?></h2>
+						<?php
+					}
+				}
 				?>
             </div>
         </div>
